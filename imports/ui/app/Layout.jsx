2f513a3ba8aa8588b594a1e432c0aadae6a7334c
rchgonzaga@gmail.com <rchgonzaga@gmail.com>
2018-04-16 23:37:42 -0300

    Setting up the main layout

---
+++ imports/ui/app/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Store from "./Store";
+
+const Layout = () => (
+  <BrowserRouter>
+    <Provider store={Store}>
+      <Switch>
+        <Route path="/" name="Home" component={Full} />
+      </Switch>
+    </Provider>
+  </BrowserRouter>
+);
+
+export default Layout;
---
+++ imports/ui/app/RootReducer.jsx
@@ -0,0 +1,21 @@
+import { combineReducers } from "redux";
+import { routerReducer } from "react-router-redux";
+
+// import studentReducer from "./views/Student/redux/reducer";
+// import schoolsReducer from "./views/Schools/redux/reducer";
+// import phoneOperators from "./views/PhoneOperators/redux/reducer";
+// import clientsReducer from "./views/Clients/redux/reducer";
+// import citiesReducer from "./views/Cities/redux/reducer";
+// import coursesReducer from "./views/Courses/redux/reducer";
+
+const reducerMap = {
+  // router: routerReducer,
+  // student: studentReducer,
+  // schools: schoolsReducer,
+  // phoneOperators: phoneOperators,
+  // clients: clientsReducer,
+  // cities: citiesReducer,
+  // courses: coursesReducer
+};
+
+export default combineReducers(reducerMap);
